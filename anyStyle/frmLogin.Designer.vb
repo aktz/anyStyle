77bf16a8cbe4db751b7f2a0e91040cdd85b7eab0
aktz <muuujica@gmail.com>
2016-03-28 11:10:24 -0500

    03282016 1110 HAS

--- anyStyle/frmLogin.Designer.vb
+++ anyStyle/frmLogin.Designer.vb
@@ -24,7 +24,6 @@ Partial Class frmLogin
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
-        Me.PictureEdit1 = New DevExpress.XtraEditors.PictureEdit()
         Me.gcLoginCompanias = New DevExpress.XtraGrid.GridControl()
         Me.grdLoginCompanias = New DevExpress.XtraGrid.Views.Grid.GridView()
         Me.comIDCompania = New DevExpress.XtraGrid.Columns.GridColumn()
@@ -34,41 +33,29 @@ Partial Class frmLogin
         Me.btnIngresar = New DevExpress.XtraEditors.SimpleButton()
         Me.txtClave = New DevExpress.XtraEditors.TextEdit()
         Me.txtUsuario = New DevExpress.XtraEditors.TextEdit()
+        Me.PictureEdit1 = New DevExpress.XtraEditors.PictureEdit()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
-        CType(Me.PictureEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.gcLoginCompanias, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.grdLoginCompanias, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.txtClave.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.txtUsuario.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
         '
         Me.PanelControl1.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.PanelControl1.Controls.Add(Me.PictureEdit1)
         Me.PanelControl1.Controls.Add(Me.gcLoginCompanias)
         Me.PanelControl1.Controls.Add(Me.btnCerrar)
         Me.PanelControl1.Controls.Add(Me.btnIngresar)
         Me.PanelControl1.Controls.Add(Me.txtClave)
         Me.PanelControl1.Controls.Add(Me.txtUsuario)
+        Me.PanelControl1.Controls.Add(Me.PictureEdit1)
         Me.PanelControl1.Location = New System.Drawing.Point(25, 25)
         Me.PanelControl1.Name = "PanelControl1"
         Me.PanelControl1.Size = New System.Drawing.Size(434, 347)
         Me.PanelControl1.TabIndex = 0
-        '
-        'PictureEdit1
-        '
-        Me.PictureEdit1.EditValue = Global.anyStyle.My.Resources.Resources.Llave
-        Me.PictureEdit1.Location = New System.Drawing.Point(22, 19)
-        Me.PictureEdit1.Name = "PictureEdit1"
-        Me.PictureEdit1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent
-        Me.PictureEdit1.Properties.Appearance.Options.UseBackColor = True
-        Me.PictureEdit1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
-        Me.PictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.[Auto]
-        Me.PictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch
-        Me.PictureEdit1.Size = New System.Drawing.Size(68, 67)
-        Me.PictureEdit1.TabIndex = 3
         '
         'gcLoginCompanias
         '
@@ -129,7 +116,7 @@ Partial Class frmLogin
         Me.btnIngresar.Appearance.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.btnIngresar.Appearance.Options.UseBackColor = True
         Me.btnIngresar.Image = CType(resources.GetObject("btnIngresar.Image"), System.Drawing.Image)
-        Me.btnIngresar.Location = New System.Drawing.Point(96, 89)
+        Me.btnIngresar.Location = New System.Drawing.Point(148, 89)
         Me.btnIngresar.LookAndFeel.SkinMaskColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.btnIngresar.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.btnIngresar.LookAndFeel.UseDefaultLookAndFeel = False
@@ -141,7 +128,7 @@ Partial Class frmLogin
         'txtClave
         '
         Me.txtClave.EditValue = ""
-        Me.txtClave.Location = New System.Drawing.Point(96, 57)
+        Me.txtClave.Location = New System.Drawing.Point(148, 57)
         Me.txtClave.Name = "txtClave"
         Me.txtClave.Properties.Appearance.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtClave.Properties.Appearance.Options.UseFont = True
@@ -152,12 +139,25 @@ Partial Class frmLogin
         'txtUsuario
         '
         Me.txtUsuario.EditValue = ""
-        Me.txtUsuario.Location = New System.Drawing.Point(96, 25)
+        Me.txtUsuario.Location = New System.Drawing.Point(148, 25)
         Me.txtUsuario.Name = "txtUsuario"
         Me.txtUsuario.Properties.Appearance.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtUsuario.Properties.Appearance.Options.UseFont = True
         Me.txtUsuario.Size = New System.Drawing.Size(156, 24)
         Me.txtUsuario.TabIndex = 0
+        '
+        'PictureEdit1
+        '
+        Me.PictureEdit1.EditValue = Global.anyStyle.My.Resources.Resources.Llave
+        Me.PictureEdit1.Location = New System.Drawing.Point(1, -2)
+        Me.PictureEdit1.Name = "PictureEdit1"
+        Me.PictureEdit1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent
+        Me.PictureEdit1.Properties.Appearance.Options.UseBackColor = True
+        Me.PictureEdit1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
+        Me.PictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.[Auto]
+        Me.PictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch
+        Me.PictureEdit1.Size = New System.Drawing.Size(203, 194)
+        Me.PictureEdit1.TabIndex = 3
         '
         'frmLogin
         '
@@ -169,11 +169,11 @@ Partial Class frmLogin
         Me.Text = "Login"
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PanelControl1.ResumeLayout(False)
-        CType(Me.PictureEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.gcLoginCompanias, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.grdLoginCompanias, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.txtClave.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.txtUsuario.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
